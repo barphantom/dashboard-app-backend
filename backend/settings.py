5c6c787c35733e8730c1d143009c7d72cc3930ea
Bartek <baarti02@gmail.com>
2025-12-01 17:17:42 +0100

    Changed django secret key

--- backend/settings.py
+++ backend/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import dj_database_url
+import os
+from dotenv import load_dotenv
 from django.conf.global_settings import CACHES
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gu9%laefi1p#3e49l6#zh^h$h6zn7!b2khh&9k5%a1^7^e73*7'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,6 +122,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url)
 
 AUTH_USER_MODEL = 'users.User'
 
